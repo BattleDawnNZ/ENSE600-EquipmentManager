5c0b469199d7da267c536d2ee539feaf77425309
cwitt <cwitt@172.29.26.16>
2025-08-04 15:18:27 +1200

    Initial Class Commit  - Defined BookingManager Class  - Defined Item Class

--- EquipmentManagerApplication/src/equipmentmanagerapplication/BookingManager.java
+++ EquipmentManagerApplication/src/equipmentmanagerapplication/BookingManager.java
@@ -1,9 +1,86 @@
 package equipmentmanagerapplication;
 
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
+ * Manages issuing and returning of items.
  *
  * @author fmw5088
  */
-public class BookingManager {
+public class BookingManager implements Serializable {
 
+    /**
+     * Contains All the Bookings. Keyed by Booking ID.
+     */
+    private HashMap<String, Booking> bookings;
+    private int currentID;
+
+    public BookingManager() {
+	bookings = new HashMap<>();
+	currentID = 0;
+    }
+
+    /**
+     * Creates a new booking.
+     *
+     * @param userID The booking users ID.
+     * @param bookedDate The date of the booking.
+     * @param returnDate The return date of the booking.
+     */
+    public void issueItem(String userID, ZonedDateTime bookedDate, ZonedDateTime returnDate) {
+	String bookingID = generateBookingID();
+	Booking booking = new Booking(bookingID, userID, bookedDate, returnDate);
+	bookings.put(bookingID, booking);
+    }
+
+    /**
+     * Remove a booking.
+     *
+     * @param bookingID The booking ID fo the booking to remove.
+     */
+    public void returnItem(String bookingID) {
+	bookings.remove(bookingID);
+    }
+
+    /**
+     *
+     * @param bookingID The booking ID of the booking requested.
+     * @return the booking with the corresponding booking ID.
+     */
+    public Booking getBooking(String bookingID) {
+	return bookings.get(bookingID);
+    }
+
+    /**
+     *
+     * @return An array copy of the bookings.
+     */
+    public Booking[] getBookings() {
+	return bookings.values().toArray(new Booking[0]);
+    }
+
+    /**
+     *
+     * @return A unique booking ID.
+     */
+    private String generateBookingID() {
+	while (bookings.containsKey(Integer.toString(currentID))) {
+	    currentID++;
+	}
+	return Integer.toString(currentID++);
+    }
+
+    @Override
+    public String toString() {
+	String output = "----- BookingManager -----\n";
+	output += "Current ID: " + currentID + "\n";
+	output += "Bookings:\n";
+	for (Map.Entry<String, Booking> entry : bookings.entrySet()) {
+	    output += " - " + entry.getValue().toString() + "\n";
+	}
+	return output;
+    }
 }
--- EquipmentManagerApplication/src/equipmentmanagerapplication/Item.java
+++ EquipmentManagerApplication/src/equipmentmanagerapplication/Item.java
@@ -1,9 +1,93 @@
 package equipmentmanagerapplication;
 
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+
 /**
  *
  * @author fmw5088
  */
-public class Item {
+public class Item implements Serializable {
 
+    private String id;
+    private String name;
+    private String description;
+    private String location;
+
+    /**
+     * Indicates the status of an item.
+     */
+    public static enum Status {
+	broken,
+	decommissioned,
+	faulty
+    }
+    private Status status;
+    private String type;
+
+    /**
+     * Represents an event in an items history.
+     */
+    public static class History {
+
+	/**
+	 *
+	 * @return The time stamp for when this event happened.
+	 */
+	private ZonedDateTime timeStamp;
+
+	/**
+	 *
+	 * @return The description of the event.
+	 */
+	private String description;
+
+	/**
+	 *
+	 * @param timeStamp When this event happened.
+	 * @param description What happened.
+	 */
+	public History(ZonedDateTime timeStamp, String description) {
+	    this.timeStamp = timeStamp;
+	    this.description = description;
+	}
+
+	/**
+	 *
+	 * @return The time stamp for when this event happened.
+	 */
+	public ZonedDateTime getTimeStamp() {
+	    return timeStamp;
+	}
+
+	/**
+	 *
+	 * @return The description of the event.
+	 */
+	public String getDescription() {
+	    return description;
+	}
+    }
+    private ArrayList<History> history;
+    private ZonedDateTime lastCalibration;
+
+    public Item(String id, String name, String description, String location, Status status, String type, ZonedDateTime lastCalibration) {
+	this.id = id;
+	this.name = name;
+	this.description = description;
+	this.location = location;
+	this.status = status;
+	this.type = type;
+	this.history = new ArrayList<History>();
+	this.lastCalibration = lastCalibration;
+    }
+
+    /**
+     *
+     * @return An array copy of the items history.
+     */
+    public History[] getHistory() {
+	return history.toArray(new History[0]);
+    }
 }
