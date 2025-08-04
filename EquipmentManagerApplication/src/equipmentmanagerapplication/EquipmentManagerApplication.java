f74258312f58b3284bc87b55c7d63a210421ac6c
cwitt <cwitt@172.29.26.16>
2025-08-04 12:15:35 +1200

    Intial Commit  - Project Created

---
+++ EquipmentManagerApplication/src/equipmentmanagerapplication/EquipmentManagerApplication.java
@@ -0,0 +1,16 @@
+package equipmentmanagerapplication;
+
+/**
+ *
+ * @author fmw5088
+ */
+public class EquipmentManagerApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
