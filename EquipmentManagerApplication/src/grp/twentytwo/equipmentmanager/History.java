5e7126caa5eb23b8d509c229fc972e79d5aa57e6
mymys <mymys@172.29.29.122>
2025-10-18 13:30:34 +1300

    HistoryManager draft complete

--- EquipmentManagerApplication/src/grp/twentytwo/equipmentmanager/History.java
+++ EquipmentManagerApplication/src/grp/twentytwo/equipmentmanager/History.java
@@ -14,25 +14,34 @@ class History {
 
     private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
-    /**
-     *
-     * @return The time stamp for when this event happened.
-     */
-    private ZonedDateTime timestamp;
+    private final String id; // Unique identifier for the history 
+    private final String itemID; // Asociated item the history is for
+    private final ZonedDateTime timestamp; // The time stamp for when this event happened.
+    private final String description; // The description of the event.
 
     /**
      *
-     * @return The description of the event.
+     * Package private constructor for official objects
      */
-    private String description;
-
-    /**
-     *
-     * @param description What happened.
-     */
-    History(String description) {
-        this.timestamp = ZonedDateTime.now();
+    History(String id, String itemID, ZonedDateTime timestamp, String description) {
+        this.id = id;
+        this.itemID = itemID;
+        this.timestamp = timestamp;
         this.description = description;
+    }
+
+    /**
+     * Public constructor for new (unsaved) history objects
+     *
+     * @param itemID
+     * @param description
+     */
+    public History(String itemID, String description) {
+        this(null, itemID, ZonedDateTime.now(), description);
+    }
+
+    public String getID() {
+        return this.id;
     }
 
     /**
@@ -43,12 +52,12 @@ class History {
         return timestamp;
     }
 
-    /**
-     *
-     * @return The description of the event.
-     */
     public String getDescription() {
         return description;
+    }
+
+    public String getItemID() {
+        return this.itemID;
     }
 
     /**
