c70297791b0b168a296acfaae2af5510ea51c3c7
mymys <mymys@172.29.29.122>
2025-10-24 20:29:29 +1300

    Test cases for getNextPrimaryKeyAdded

--- EquipmentManagerApplication/test/grp/twentytwo/database/TableManagerTest.java
+++ EquipmentManagerApplication/test/grp/twentytwo/database/TableManagerTest.java
@@ -8,13 +8,15 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.sql.ResultSet;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
+ * This JUNIT class tests key functionality of the table manager. Eg. create,
+ * update delete and get rows. Low-level functions are inheritely tested
+ * therefor focus is on these main functions.
  *
  * @author mymys
  */
@@ -30,40 +32,33 @@ public class TableManagerTest {
     public TableManagerTest() {
     }
 
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
     @Before
     public void setUp() {
         try {
             dbManager = new DatabaseManager("pdc", "pdc", "jdbc:derby:EquipmentManagerDB; create=true");
             dbManager.dropTableIfExists(tableName);
-            columns = new ArrayList<Column>();
-            column_primaryKey = new Column("Column1", "VARCHAR(12)", "key");
+            columns = new ArrayList<>();
+            column_primaryKey = new Column("Column1", "VARCHAR(12)", "20");
             column_dataColumn = new Column("Column2", "VARCHAR(12)", "data2");
             columns.add(column_primaryKey);
             columns.add(column_dataColumn);
             tableManager = new TableManager(dbManager, tableName, columns, column_primaryKey);
             tableManager.createRow(columns);
-        } catch (Exception ex) {
+        } catch (DatabaseConnectionException | InvalidColumnNameException | NullColumnValueException | PrimaryKeyClashException ex) {
             Logger.getLogger(TableManagerTest.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
+    // ------------------ CREATE ROW TESTS -------------------- \\
     @Test(expected = PrimaryKeyClashException.class)
-    public void testCreateRow_duplicateKey() throws InvalidColumnNameException, PrimaryKeyClashException, SQLException, NullColumnValueException {
+    public void testCreateRow_duplicateKey() throws InvalidColumnNameException, PrimaryKeyClashException, NullColumnValueException {
         System.out.println("TEST: Create row, duplicate key");
+        tableManager.createRow(columns);
         tableManager.createRow(columns);
     }
 
     @Test(expected = NullColumnValueException.class)
-
-    public void testCreateRow_nullKey() throws InvalidColumnNameException, PrimaryKeyClashException, SQLException, NullColumnValueException {
+    public void testCreateRow_nullKey() throws InvalidColumnNameException, PrimaryKeyClashException, NullColumnValueException {
         System.out.println("TEST: Create row, Null primary key");
         column_primaryKey.data = null;
         tableManager.createRow(columns);
@@ -75,9 +70,130 @@ public class TableManagerTest {
         // Create new, different column array
         ArrayList<Column> newArr = new ArrayList<>();
         newArr.add(new Column("Column1", "VARCHAR(12)", "key"));
-        newArr.add(new Column("Column2", "VARCHAR(12)", "587")); // Data here is different
-        newArr.add(new Column("Column5", "VARCHAR(12)", "data1"));
+        newArr.add(new Column("Column2", "VARCHAR(12)", "587"));
+        newArr.add(new Column("Column5", "VARCHAR(12)", "data1")); // Data here is different
         tableManager.createRow(newArr);
+    }
+
+    @Test
+    public void testCreateRow_valid() throws InvalidColumnNameException, PrimaryKeyClashException, NullColumnValueException {
+        System.out.println("TEST: Create row, valid column names and data");
+        column_primaryKey.data = "1234"; // Unique, non-duplicate key
+        tableManager.createRow(columns);
+    }
+
+    // ------------------ GET ROW TESTS -------------------- \\
+    @Test
+    public void testGetRowByPrimaryKey_valid() throws UnfoundPrimaryKeyException {
+        System.out.println("TEST: Get row, valid primaryKey");
+        tableManager.getRowByPrimaryKey(column_primaryKey.data);
+    }
+
+    @Test(expected = UnfoundPrimaryKeyException.class)
+    public void testGetRowByPrimaryKey_invalidKey() throws UnfoundPrimaryKeyException {
+        System.out.println("TEST: Get row, non-existent primaryKey");
+        tableManager.getRowByPrimaryKey("989"); // Invalid Key
+    }
+
+    @Test(expected = UnfoundPrimaryKeyException.class)
+    public void testGetRowByPrimaryKey_nullKey() throws UnfoundPrimaryKeyException {
+        System.out.println("TEST: Get row, null primaryKey");
+        tableManager.getRowByPrimaryKey(null); // Invalid Key
+    }
+
+    // ------------------ DELETE ROW TESTS -------------------- \\
+    @Test
+    public void testDeleteRowByPrimaryKey_valid() throws UnfoundPrimaryKeyException, SQLException {
+        System.out.println("TEST: Delete row, valid primaryKey");
+        boolean result = tableManager.deleteRowByPrimaryKey(column_primaryKey.data);
+        boolean verify = false;
+        if (!tableManager.verifyPrimaryKey(column_primaryKey.data)) {
+            verify = true;
+        }
+        assert (result && verify);
+    }
+
+    @Test(expected = UnfoundPrimaryKeyException.class)
+    public void testDeleteRowByPrimaryKey_invalidKey() throws UnfoundPrimaryKeyException {
+        System.out.println("TEST: Delete row, non-existent primaryKey");
+        tableManager.deleteRowByPrimaryKey("242");
+    }
+
+    @Test(expected = UnfoundPrimaryKeyException.class)
+    public void testDeleteRowByPrimaryKey_nullKey() throws UnfoundPrimaryKeyException {
+        System.out.println("TEST: Delete row, null primaryKey");
+        tableManager.deleteRowByPrimaryKey(null); // Invalid Key
+    }
+
+    // ------------------ UPDATE ROW TESTS -------------------- \\
+    @Test
+    public void testUpdateRowByPrimaryKey_valid() throws UnfoundPrimaryKeyException, SQLException, InvalidColumnNameException {
+        System.out.println("TEST: Update row, valid primaryKey and data");
+        column_dataColumn.data = "22";
+        tableManager.updateRowByPrimaryKey(columns);
+        ResultSet newVal = tableManager.getRowByPrimaryKey(column_primaryKey.data);
+        if (newVal.next()) {
+            assert (newVal.getString("Column2").equals("22"));
+        }
+    }
+
+    @Test(expected = UnfoundPrimaryKeyException.class)
+    public void testUpdateRowByPrimaryKey_invalidKey() throws UnfoundPrimaryKeyException, InvalidColumnNameException {
+        System.out.println("TEST: Update row, non-existent primaryKey");
+        column_primaryKey.data = "242";
+        tableManager.updateRowByPrimaryKey(columns);
+    }
+
+    @Test(expected = UnfoundPrimaryKeyException.class)
+    public void testUpdateRowByPrimaryKey_nullKey() throws UnfoundPrimaryKeyException, InvalidColumnNameException {
+        System.out.println("TEST: Update row, null primaryKey");
+        column_primaryKey.data = null;
+        tableManager.updateRowByPrimaryKey(columns);
+    }
+
+    @Test(expected = InvalidColumnNameException.class)
+    public void testUpdateRowByPrimaryKey_invalidColumn() throws InvalidColumnNameException, UnfoundPrimaryKeyException {
+        System.out.println("TEST: Update row, Invalid column definitions");
+        // Create new, different column array
+        ArrayList<Column> newArr = new ArrayList<>();
+        newArr.add(new Column("Column1", "VARCHAR(12)", "key"));
+        newArr.add(new Column("Column2", "VARCHAR(12)", "587"));
+        newArr.add(new Column("Column5", "VARCHAR(12)", "data1")); // Data here is different
+        tableManager.updateRowByPrimaryKey(newArr);
+    }
+
+    // ------------------ GET NEXT PRIMARY KEY TESTS -------------------- \\
+    @Test
+    public void testGetNextPrimaryKey_numericalKey() throws UnfoundPrimaryKeyException, DatabaseConnectionException, DatabaseConnectionException, DatabaseConnectionException, DatabaseConnectionException, DatabaseConnectionException {
+        System.out.println("TEST: Get next primary key, numerical first entry");
+        String nextKey = tableManager.getNextPrimaryKeyId();
+        assert (nextKey.equals("21"));
+    }
+
+    @Test
+    public void testGetNextPrimaryKey_nonNumericKey() throws UnfoundPrimaryKeyException, DatabaseConnectionException, InvalidColumnNameException, PrimaryKeyClashException, NullColumnValueException {
+        System.out.println("TEST: Get next primary key, alphabetical first entry");
+        column_primaryKey.data = "h";
+        tableManager.createRow(columns);
+        String nextKey = tableManager.getNextPrimaryKeyId();
+        assert (nextKey.equals("0"));
+    }
+
+    @Test
+    public void testGetNextPrimaryKey_partialNumericKey() throws UnfoundPrimaryKeyException, DatabaseConnectionException, InvalidColumnNameException, PrimaryKeyClashException, NullColumnValueException {
+        System.out.println("TEST: Get next primary key, alphabetical first entry");
+        column_primaryKey.data = "h2";
+        tableManager.createRow(columns);
+        String nextKey = tableManager.getNextPrimaryKeyId();
+        assert (nextKey.equals("3"));
+    }
+
+    @Test
+    public void testGetNextPrimaryKey_emptyTable() throws UnfoundPrimaryKeyException, DatabaseConnectionException, InvalidColumnNameException, PrimaryKeyClashException, NullColumnValueException {
+        System.out.println("TEST: Get next primary key, alphabetical first entry");
+        tableManager.deleteRowByPrimaryKey(column_primaryKey.data);
+        String nextKey = tableManager.getNextPrimaryKeyId();
+        assert (nextKey.equals("0"));
     }
 
     @After
