e7f68b153d83835383a3ffec148d1d7fbaeeabfd
mymys <mymys@172.29.29.122>
2025-10-30 12:00:27 +1300

    TestData made non-runnable

--- EquipmentManagerApplication/src/grp/twentytwo/equipmentmanager/TestData.java
+++ EquipmentManagerApplication/src/grp/twentytwo/equipmentmanager/TestData.java
@@ -13,87 +13,86 @@ public class TestData {
 
     private static ModelManager model = new ModelManager();
 
-    public static void main(String[] args) {
-	TestData.model.setupManagers();
-	TestData.dropTables();
-	TestData.model.setupManagers();
-	TestData.generate();
-	TestData.printAll();
-    }
-
+//    public static void main(String[] args) {
+//	TestData.model.setupManagers();
+//	TestData.dropTables();
+//	TestData.model.setupManagers();
+//	TestData.generate();
+//	TestData.printAll();
+//    }
     public static void generate() {
-	try {
+        try {
 
-	    model.addLocation("WORKSHOP 1");
-	    model.addLocation("WORKSHOP 2");
-	    model.addLocation("WORKSHOP 3");
-	    model.addLocation("WORKSHOP 4");
-	    model.addLocation("WORKSHOP 5");
-	    model.addLocation("ELECTRICAL LAB 1");
-	    model.addLocation("ELECTRICAL LAB 2");
-	    model.addLocation("ELECTRICAL LAB 3");
-	    model.addLocation("ELECTRICAL LAB 4");
+            model.addLocation("WORKSHOP 1");
+            model.addLocation("WORKSHOP 2");
+            model.addLocation("WORKSHOP 3");
+            model.addLocation("WORKSHOP 4");
+            model.addLocation("WORKSHOP 5");
+            model.addLocation("ELECTRICAL LAB 1");
+            model.addLocation("ELECTRICAL LAB 2");
+            model.addLocation("ELECTRICAL LAB 3");
+            model.addLocation("ELECTRICAL LAB 4");
 
-	    User user = new Manager("111", "Bob", "123123");
-	    model.addUser(user);
-	    model.addUser(new Employee("222", "Sally", "123123"));
-	    model.addUser(new Guest("333", "Fred", "123123"));
+            User user = new Manager("111", "Bob", "123123");
+            model.addUser(user);
+            model.addUser(new Employee("222", "Sally", "123123"));
+            model.addUser(new Guest("333", "Fred", "123123"));
 
-	    model.login(user);
+            model.login(user);
 
-	    model.addItem(new Item("R9000 Universal Laser Cutter", "WORKSHOP 1", "Manufacturing/Cutting"));
-	    model.addItem(new Item("R9000 Universal Laser Cutter", "WORKSHOP 2", "Manufacturing/Cutting"));
-	    model.addItem(new Item("Ender 3D Printer", "WORKSHOP 2", "Manufacturing/Additive"));
-	    model.addItem(new Item("Ender 3D Printer", "WORKSHOP 2", "Manufacturing/Additive"));
-	    model.addItem(new Item("Ender 3D Printer", "WORKSHOP 3", "Manufacturing/Additive"));
-	    model.addItem(new Item("Ender 3D Printer", "WORKSHOP 3", "Manufacturing/Additive"));
-	    model.addItem(new Item("Ender 3D Printer", "WORKSHOP 3", "Manufacturing/Additive"));
-	    model.addItem(new Item("Duratech True RMS Multimeter", "Electrical Lab 1", "Electrical/Measurement"));
-	    model.addItem(new Item("Duratech True RMS Multimeter", "Electrical Lab 2", "Electrical/Measurement"));
-	    model.addItem(new Item("Duratech True RMS Multimeter", "Electrical Lab 4", "Electrical/Measurement"));
-	    model.addItem(new Item("Duratech True RMS Multimeter", "WORKSHOP 3", "Electrical/Measurement"));
-	    model.addItem(new Item("Duratech True RMS Multimeter", "WORKSHOP 4", "Electrical/Measurement"));
-	    model.addItem(new Item("Duratech True RMS Multimeter", "WORKSHOP 4", "Electrical/Measurement"));
-	    model.addItem(new Item("Soldering Iron", "ELECTRICAL LAB 1", "Electrical/Soldering"));
-	    model.addItem(new Item("Soldering Iron", "ELECTRICAL LAB 2", "Electrical/Soldering"));
-	    model.addItem(new Item("Soldering Iron", "ELECTRICAL LAB 2", "Electrical/Soldering"));
-	    model.addItem(new Item("Soldering Iron", "ELECTRICAL LAB 3", "Electrical/Soldering"));
-	    model.addItem(new Item("Soldering Iron", "ELECTRICAL LAB 3", "Electrical/Soldering"));
-	    model.addItem(new Item("Soldering Iron", "WORKSHOP 4", "Electrical/Soldering"));
-	    model.addItem(new Item("Soldering Iron", "WORKSHOP 5", "Electrical/Soldering"));
+            model.addItem(new Item("R9000 Universal Laser Cutter", "WORKSHOP 1", "Manufacturing/Cutting"));
+            model.addItem(new Item("R9000 Universal Laser Cutter", "WORKSHOP 2", "Manufacturing/Cutting"));
+            model.addItem(new Item("Ender 3D Printer", "WORKSHOP 2", "Manufacturing/Additive"));
+            model.addItem(new Item("Ender 3D Printer", "WORKSHOP 2", "Manufacturing/Additive"));
+            model.addItem(new Item("Ender 3D Printer", "WORKSHOP 3", "Manufacturing/Additive"));
+            model.addItem(new Item("Ender 3D Printer", "WORKSHOP 3", "Manufacturing/Additive"));
+            model.addItem(new Item("Ender 3D Printer", "WORKSHOP 3", "Manufacturing/Additive"));
+            model.addItem(new Item("Duratech True RMS Multimeter", "Electrical Lab 1", "Electrical/Measurement"));
+            model.addItem(new Item("Duratech True RMS Multimeter", "Electrical Lab 2", "Electrical/Measurement"));
+            model.addItem(new Item("Duratech True RMS Multimeter", "Electrical Lab 4", "Electrical/Measurement"));
+            model.addItem(new Item("Duratech True RMS Multimeter", "WORKSHOP 3", "Electrical/Measurement"));
+            model.addItem(new Item("Duratech True RMS Multimeter", "WORKSHOP 4", "Electrical/Measurement"));
+            model.addItem(new Item("Duratech True RMS Multimeter", "WORKSHOP 4", "Electrical/Measurement"));
+            model.addItem(new Item("Soldering Iron", "ELECTRICAL LAB 1", "Electrical/Soldering"));
+            model.addItem(new Item("Soldering Iron", "ELECTRICAL LAB 2", "Electrical/Soldering"));
+            model.addItem(new Item("Soldering Iron", "ELECTRICAL LAB 2", "Electrical/Soldering"));
+            model.addItem(new Item("Soldering Iron", "ELECTRICAL LAB 3", "Electrical/Soldering"));
+            model.addItem(new Item("Soldering Iron", "ELECTRICAL LAB 3", "Electrical/Soldering"));
+            model.addItem(new Item("Soldering Iron", "WORKSHOP 4", "Electrical/Soldering"));
+            model.addItem(new Item("Soldering Iron", "WORKSHOP 5", "Electrical/Soldering"));
 
-	    model.addNote("E00000", "Test note");
-	    model.addNote("E00000", "Test note 2");
-	    model.addNote("E00001", "Test note");
-	    model.addNote("E00001", "Test note");
+            model.addNote("E00000", "Test note");
+            model.addNote("E00000", "Test note 2");
+            model.addNote("E00001", "Test note");
+            model.addNote("E00001", "Test note");
 
-	    LocalDateTime dateBooked = LocalDateTime.of(2025, 10, 5, 8, 0); // 5/10/2025 8:00am 
-	    LocalDateTime dateReturned = LocalDateTime.of(2025, 10, 5, 8, 30); // 5/10/2025 8:30am
-	    model.addBooking(new Booking("111", "E00000", dateBooked, dateReturned));
-	    model.addBooking(new Booking("111", "E00000", dateReturned.plusDays(2), dateReturned.plusDays(3)));
-	    model.addBooking(new Booking("222", "E00001", dateBooked, dateReturned));
-	    model.addBooking(new Booking("222", "E00001", dateBooked, dateReturned));
-	} catch (Exception ex) {
-	    Logger.getLogger(TestData.class.getName()).log(Level.SEVERE, null, ex);
-	}
+            LocalDateTime dateBooked = LocalDateTime.of(2025, 10, 5, 8, 0); // 5/10/2025 8:00am 
+            LocalDateTime dateReturned = LocalDateTime.of(2025, 10, 5, 8, 30); // 5/10/2025 8:30am
+            model.addBooking(new Booking("111", "E00000", dateBooked, dateReturned));
+            model.addBooking(new Booking("111", "E00000", dateReturned.plusDays(2), dateReturned.plusDays(3)));
+            model.addBooking(new Booking("222", "E00001", dateBooked, dateReturned));
+            model.addBooking(new Booking("222", "E00001", dateBooked, dateReturned));
+        } catch (Exception ex) {
+            Logger.getLogger(TestData.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
     public static void printAll() {
-	model.itemManager.printTable();
-	model.locationManager.printTable();
-	model.userManager.printTable();
-	model.bookingManager.printTable();
+        model.itemManager.printTable();
+        model.locationManager.printTable();
+        model.userManager.printTable();
+        model.bookingManager.printTable();
 
     }
 
     public static void dropTables() {
-	model.databaseManager.dropTableIfExists("HISTORYTABLE");
-	model.databaseManager.dropTableIfExists("BOOKINGTABLE");
-	model.databaseManager.dropTableIfExists("LOCATIONTABLE");
-	model.databaseManager.dropTableIfExists("ITEMTABLE");
-	model.databaseManager.dropTableIfExists("USERTABLE");
-	model = new ModelManager();
+        model.databaseManager.dropTableIfExists("HISTORYTABLE");
+        model.databaseManager.dropTableIfExists("BOOKINGTABLE");
+        model.databaseManager.dropTableIfExists("LOCATIONTABLE");
+        model.databaseManager.dropTableIfExists("ITEMTABLE");
+        model.databaseManager.dropTableIfExists("USERTABLE");
+        model = new ModelManager();
     }
 
 }
